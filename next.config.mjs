6e7a6f3810a8c8b707f3f0114b0f53e0e7e858f7
DimzTheGenshiro <oinzsreal@gmail.com>
2025-09-18 01:04:58 +0700

    Fixing build errors on Vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  swcMinify: false, // Menonaktifkan SWC
-  compiler: {
-    // Gunakan Babel sebagai pengganti SWC
-    // Perlu instal @babel/core dan @babel/preset-env
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
 
